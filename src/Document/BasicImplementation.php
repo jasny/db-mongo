cd6eeaa77a68b25a4bf8956c71d506f0eb6ccef3
moesjarraf <moesjarraf@gmail.com>
2016-07-14 12:56:53 +0200

    Use castForDB when deleting

--- src/Document/BasicImplementation.php
+++ src/Document/BasicImplementation.php
@@ -148,7 +148,8 @@ trait BasicImplementation
      */
     public function delete(array $opts = [])
     {
-        $filter = [static::getIdProperty() => $this->childEntityToId($this)];
+        $properties = [static::getIdProperty() => $this->getId()];
+        $filter = static::castForDB($properties);
         $query = static::mapToFields($filter);
         
         static::getCollection()->remove($query);
