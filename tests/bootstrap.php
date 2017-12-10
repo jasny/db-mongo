7efd455706390daba4ab290357f175464a7385c8
Sergey Boltonosov <minstel@yandex.ru>
2017-12-10 07:29:57 +0700

    Added tests for Document-MetaImplementation trait

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,12 @@
 
 require_once "vendor/autoload.php";
 require_once "tests/support/TestHelper.php";
+
 require_once "tests/support/stubs/TestEntity.php";
 require_once "tests/support/stubs/TestEntityLazy.php";
 require_once "tests/support/stubs/TestDocumentLazy.php";
 require_once "tests/support/stubs/TestDocumentSorted.php";
+require_once "tests/support/stubs/TestDocumentMeta.php";
+require_once "tests/support/stubs/TestDocumentMetaEmpty.php";
 require_once "tests/support/stubs/TestEntityMetaMongo.php";
 require_once "tests/support/stubs/TestEntityMeta.php";
